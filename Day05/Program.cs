a48792fe5022a09adb0e1e22d802b8f8e772ded6
kry.ban <kry.ban@gmail.com>
2021-12-20 15:48:25 +0100

    Day 5 part 2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -4,13 +4,17 @@ Console.WriteLine("Hello, World!");
 string inputFile = @"./ProjectItems/input.txt";
 var inputRaw = File.ReadAllLines(inputFile).ToList();
 
-List<Line> relevantLines = ImportHorizontalOrVerticalLines(inputRaw);
+List<Line> relevantLinesPart1 = ImportRelevantLines(1,inputRaw);
+List<Line> relevantLinesPart2 = ImportRelevantLines(2, inputRaw);
 
-List<(int x, int y)> linePoints = CollectCoveredPoints(relevantLines);
+List<(int x, int y)> linePointsPart1 = CollectCoveredPoints(relevantLinesPart1);
+List<(int x, int y)> linePointsPart2 = CollectCoveredPoints(relevantLinesPart2);
 
-int answer = CountMatches(linePoints);
+int answer1 = CountMatches(linePointsPart1);
+int answer2 = CountMatches(linePointsPart2);
 
-Console.WriteLine($"Answer Day 5, Part 1: {answer}");
+Console.WriteLine($"Answer Day 5, Part 1: {answer1}");
+Console.WriteLine($"Answer Day 5, Part 2: {answer2}");
 Console.ReadKey();
 
 int CountMatches(List<(int x, int y)> linePoints)
@@ -32,15 +36,15 @@ List<(int x, int y)> CollectCoveredPoints(List<Line> relevantLines)
     return retval;
 }
 
-List<Line> ImportHorizontalOrVerticalLines(List<string> inputRaw)
+List<Line> ImportRelevantLines(int part, List<string> inputRaw)
 {
     List<Line> retval = new List<Line>();
 
     var cleanedInput = inputRaw.Select(x => ReplaceArrow(x)).ToList();
 
-    cleanedInput.ForEach(line => retval.Add(CreateLineFromInput(line)));
+    cleanedInput.ForEach(line => retval.Add(CreateLineFromInput(part, line)));
 
-    retval = FilterVerticalOrHorizontalOnly(retval);
+    retval = FilterForRelevantLines(part,retval);
 
     return retval;
 }
@@ -50,7 +54,7 @@ string ReplaceArrow(string input)
     return input.Trim().Replace(" ", string.Empty).Replace("->", ",");
 }
 
-Line CreateLineFromInput(string input)
+Line CreateLineFromInput(int part, string input)
 {
     var points = input.Split(',').Select(x => int.Parse(x)).ToList();
 
@@ -61,11 +65,13 @@ Line CreateLineFromInput(string input)
         endX = points[2],
         endY = points[3],
 
-        series = SetLineSerie(points[0], points[1], points[2], points[3])
+        series = part == 1 
+        ? SetLineSeriePart2(points[0], points[1], points[2], points[3])
+        : SetLineSeriePart2(points[0], points[1], points[2], points[3])
     };
 }
 
-List<(int x, int y)> SetLineSerie(int startX, int startY, int endX, int endY)
+List<(int x, int y)> SetLineSeriePart1(int startX, int startY, int endX, int endY)
 {   
     var retval = new List<(int x,int y)>();
 
@@ -107,20 +113,122 @@ List<(int x, int y)> SetLineSerie(int startX, int startY, int endX, int endY)
     return retval;
 }
 
-static List<Line> FilterVerticalOrHorizontalOnly(List<Line> unfiltered)
+List<(int x, int y)> SetLineSeriePart2(int startX, int startY, int endX, int endY)
+{
+    var retval = new List<(int x, int y)>();
+    //var difference = Math.Abs(startX - endX);
+    var sum = startX + startY;
+
+    if (startX < endX && startY < endY)
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX + i, startY + i));
+        }
+    }
+
+    else if (startX < endX && startY > endY)
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX + i, startY - i));
+        }
+    }
+
+    else if (startX < endX && (startY == endY))
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX + i, startY));
+        }
+    }
+
+    else if (startX > endX && startY > endY)
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX - i, startY - i));
+        }
+    }
+
+    else if (startX > endX && startY < endY)
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX - i, startY + i));
+        }
+    }
+
+    else if (startX > endX && (startY == endY))
+    {
+        for (int i = 0; i <= Math.Abs(startX - endX); i++)
+        {
+            retval.Add((startX - i, startY));
+        }
+    }
+
+    else if ((startX == endX) && startY > endY)
+    {
+        for (int i = 0; i <= Math.Abs(startY - endY); i++)
+        {
+            retval.Add((startX, startY-i));
+        }
+    }
+    else if ((startX == endX) && startY < endY)
+    {
+        for (int i = 0; i <= Math.Abs(startY - endY); i++)
+        {
+            retval.Add((startX, startY + i));
+        }
+    }
+    else
+    {
+            retval.Add((startX, startY));
+    }
+
+    return retval;
+}
+
+List<Line> FilterForRelevantLines(int part, List<Line> unfiltered)
 {
     var retval = new List<Line>(unfiltered);
 
     foreach (var line in unfiltered)
     {
-        if (line.startX != line.endX && line.startY != line.endY)
+        if (part == 1)
         {
-            retval.Remove(line);
+            if (!HasSameX(line) && !HasSameY(line))
+            {
+                retval.Remove(line);
+            }
+        }
+        else 
+        {
+            if (!HasSameX(line) && !HasSameY(line) && !Is45Degrees(line))
+            {
+                retval.Remove(line);
+            }
         }
     }
 
     return retval;
 }
+
+bool Is45Degrees(Line line)
+{
+    return (Math.Abs(line.startX - line.endX)) == (Math.Abs(line.startY - line.endY));
+}
+
+bool HasSameX(Line line)
+{
+    return line.startX == line.endX;
+}
+
+bool HasSameY(Line line)
+{
+    return line.startY == line.endY;
+}
+
 internal class Line
 {
     public int startX{ get; set; }
@@ -128,5 +236,5 @@ internal class Line
     public int endX { get; set; }
     public int endY { get; set; }
 
-    public List<(int x,int y)> series { get; set; }
+    public List<(int x, int y)> series { get; set; }
 }
