74cf6105cfe43f99ce6d3b8983674765b27c7703
kry.ban <kry.ban@gmail.com>
2021-12-20 20:33:03 +0100

    Day 6, part 1 solution

---
+++ Day06/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string inputFile = @"./ProjectItems/input.txt";
+var inputRaw = File.ReadAllLines(inputFile).ToList();
+var initialFishes = inputRaw.First().Split(',').Select(x => new Fish(int.Parse(x))).ToList();
+var duration = 80;
+var population = new List<Fish>(initialFishes);
+
+for (int i = 0; i < duration; i++)
+{
+    foreach (var fish in new List<Fish>(population))
+    {
+        if(fish.Timer == 0)
+        {
+            fish.Timer = 6;
+            population.Add(new Fish());
+            continue;
+        }
+
+        fish.Timer--;
+    }
+}
+
+Console.WriteLine($"Total days easured: {duration}");
+Console.WriteLine($"Answer day 5, part 1: {population.Count}");
+Console.ReadKey();
+
+class Fish
+{
+    public Fish()
+    {
+        Timer = 8;
+    }
+    public Fish(int timer)
+    {
+        Timer = timer;
+    }
+    public int Timer { get; set; }
+}
+
