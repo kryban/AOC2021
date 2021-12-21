661399289ce5150eae956b4b1983716a64fb7c0f
kry.ban <kry.ban@gmail.com>
2021-12-21 22:54:24 +0100

    Day 7, part 2

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,16 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string inputFilePart1 = @"./ProjectItems/input.txt";
+string inputFilePart = @"./ProjectItems/input.txt";
 
-
-long CalculateFuel(string inputFile)
+(long position, long costs) CalculateFuelWithConstantBurnRate(string inputFile)
 {
     long retval = 0;
     var inputRaw = File.ReadAllLines(inputFile).ToList();
     var crabsUnpositioned = inputRaw.First().Split(',').Select(x => long.Parse(x)).ToList();
 
-    List<long> allRouteCosts = new List<long>();
+    List<(long position, long fuelCosts)> allRouteCosts = new List<(long position, long fuelCosts)>();
 
     foreach(var crab in crabsUnpositioned)
     {
@@ -20,11 +19,49 @@ long CalculateFuel(string inputFile)
             subtotal += Math.Abs(crabsUnpositioned[i] - crab);
         }
 
-        allRouteCosts.Add(subtotal);
+        allRouteCosts.Add((crab,subtotal));
     }
 
-    return allRouteCosts.Min();
+    return allRouteCosts.OrderBy(f =>f.fuelCosts).First();
 }
 
-Console.WriteLine($"Day 7, part 1: {CalculateFuel(inputFilePart1)}");
+(long position, long costs) CalculateFuelWithIncreasingBurnRate(string inputFile)
+{
+    var inputRaw = File.ReadAllLines(inputFile).ToList();
+    var crabsUnpositioned = inputRaw.First().Split(',').Select(x => int.Parse(x)).ToList();
+
+    List<(long position, long fuelCosts)> allRouteCosts = new List<(long position, long fuelCosts)>();
+
+    var allPositions = crabsUnpositioned.Max() - crabsUnpositioned.Min();
+
+    for (int currentPosition = crabsUnpositioned.Min(); currentPosition < allPositions; currentPosition++)
+    {
+        long subtotal = 0;
+
+        foreach (var crab in crabsUnpositioned)
+        {
+            var distance = Math.Abs(currentPosition - crab);
+            var subcosts = 0;
+
+            if (distance > 0)
+            {
+                for (int l = 1; l <= distance; l++)
+                {
+                    subcosts += l;
+                }
+
+                subtotal += subcosts;
+            }
+        }
+
+        allRouteCosts.Add((currentPosition, subtotal));
+    }
+
+    return allRouteCosts.OrderBy(f => f.fuelCosts).First();
+}
+
+var part1 = CalculateFuelWithConstantBurnRate(inputFilePart);
+var part2 = CalculateFuelWithIncreasingBurnRate(inputFilePart);
+Console.WriteLine($"Day 7, part 1: Cheapest move is to position {part1.position} costs {part1.costs} fuel.");
+Console.WriteLine($"Day 7, part 2: Cheapest move is to position {part2.position} costs {part2.costs} fuel.");
 Console.ReadKey();
