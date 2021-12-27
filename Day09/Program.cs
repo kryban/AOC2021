1f77fee0207ab9d753ae4ffcfa8a9988dec76534
kry.ban <kry.ban@gmail.com>
2021-12-27 11:06:43 +0100

    Day 9, part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -5,65 +5,42 @@ string inputFile = @"./ProjectItems/input.txt";
 List<string> inputRaw = File.ReadAllLines(inputFile).ToList();
 List<PartPoint> lowestPoints = new List<PartPoint>();
 
+int CalculatePart1(List<string> inputRaw)
+{
+    lowestPoints = CalculateLowest(inputRaw);
+    return CalculateLowest(inputRaw).Select(x => Convert.ToInt32(x.CurrentValue) + 1).ToList().Sum();
+}
 
-int Part2()
+int CalculatePart2()
 {
     int retval = 0;
     List<int> relevantNeighbours = new List<int>();
 
-    //1 calculate lowest 
     List<PartPoint> lowestPoints = CalculateLowest(inputRaw);
-    List<int> bassinSizes = new List<int>();
+    List<(PartPoint bassin, int size)> bassinSizes = new List<(PartPoint bassin, int size)>();
 
-    //1 get first lowest 
     foreach (var lowestPoint in lowestPoints)
     {
         List<PartPoint> unhandledPoints = new List<PartPoint>();
+        List<PartPoint> handledPoints = new List<PartPoint>();
 
-        if(lowestPoint.PartOfBassin)
+        handledPoints.Add(lowestPoint);
+
+        AddNeighboursToUnhandled(handledPoints, unhandledPoints, lowestPoint);
+
+        while (unhandledPoints.Count > 0)
         {
-            unhandledPoints.Add(lowestPoint);
+            var pointInProcess = unhandledPoints.First();
+            AddNeighboursToUnhandled(handledPoints, unhandledPoints, pointInProcess);
+            handledPoints.Add(pointInProcess);
+            unhandledPoints.Remove(pointInProcess);
         }
 
-
-
-        for (int i = 0; i < inputRaw.Count(); i++)
-        {
-            var currentRowPosition = i;
-
-            for (int ii = 0; ii < inputRaw[currentRowPosition].Length; ii++)
-            {
-                int currentPosition = ii;
-
-                var p = new PartPoint(inputRaw, currentRowPosition, currentPosition);
-
-                if (p.IsSmallest)
-                    unhandledPoints.Add(p);
-            }
-        }
-        //2 calculate neighbours
-        //  and add to unhandled neighbours
+        bassinSizes.Add((lowestPoint, handledPoints.Count));
     }
 
-
-    //3 get first unhandled neighbour
-    //calculate neighbours
-    //add neighbour to unhandled neighbours if it is greater then current but not lowest of 9
-    //remove current from unhnadled and add to handled 
-
-    //4 repeat 3
-
-    //4 if there is no more unhandled neighobour,
-    //count all numbers in bassin
-    //write result to bassin size
-
-    //repeat step 1
-
-    // if there is no lowest
-    //select top 3 bassins
-    //multiply size of the 3 bassins
-    var result = bassinSizes.OrderByDescending(x => x).ToList().Take(3).ToList();
-    retval = result[0] * result[1] * result[3];
+    var result = bassinSizes.OrderByDescending(x => x.size).ToList().Take(3).ToList();
+    retval = result[0].size * result[1].size * result[2].size;
 
     return retval;
 }
@@ -71,13 +48,8 @@ int Part2()
 CalculateLowest(inputRaw).ForEach(p => Console.Write(p.CurrentValue));
 Console.WriteLine();
 Console.WriteLine($"Day 9, part 1: {CalculatePart1(inputRaw)}");
+Console.WriteLine($"Day 9, part 2: {CalculatePart2()}");
 Console.ReadKey();
-
-int CalculatePart1(List<string> inputRaw)
-{
-    lowestPoints = CalculateLowest(inputRaw);
-    return CalculateLowest(inputRaw).Select(x => Convert.ToInt32(x.CurrentValue) + 1).ToList().Sum();
-}
 
 List<PartPoint> CalculateLowest(List<string> inputRaw)
 {
@@ -99,11 +71,23 @@ List<PartPoint> CalculateLowest(List<string> inputRaw)
     return retval;
 }
 
+void AddNeighboursToUnhandled(List<PartPoint> handledPoints, List<PartPoint> unhandledPoints, PartPoint partPoint)
+{
+    if (partPoint.Above.IsRelevant(handledPoints, unhandledPoints))
+        unhandledPoints.Add(partPoint.Above);
+    if (partPoint.Left.IsRelevant(handledPoints, unhandledPoints))
+        unhandledPoints.Add(partPoint.Left);
+    if (partPoint.Right.IsRelevant(handledPoints, unhandledPoints))
+        unhandledPoints.Add(partPoint.Right);
+    if (partPoint.Below.IsRelevant(handledPoints, unhandledPoints))
+        unhandledPoints.Add(partPoint.Below);
+}
+
 public class PartPoint
 {
     private List<string> inputRaw = new List<string>();
-    private int currentRowPosition;
-    private int currentPosition;
+    public int currentRowPosition;
+    public int currentPosition;
 
     public PartPoint(List<string> inputRaw, int currentRowPosition, int currentPosition)
     {
@@ -126,27 +110,26 @@ public class PartPoint
 
     public bool IsSmallest => CurrentValue.IsSmallerThen(Above?.CurrentValue) && CurrentValue.IsSmallerThen(Left?.CurrentValue)
         && CurrentValue.IsSmallerThen(Right?.CurrentValue) && CurrentValue.IsSmallerThen(Below?.CurrentValue);
-    public bool PartOfBassin => Convert.ToInt32(CurrentValue) < 9;
-
-    private PartPoint? GetPointIfRelevant(List<string> inputRaw, int neighbourPosition, int currentPosition, string currentValue)
-    {
-        var neighbour = new PartPoint(inputRaw, neighbourPosition, currentPosition);
-        var neighbourValue = Convert.ToInt32(neighbour.CurrentValue);
-
-        if (neighbourValue > Convert.ToInt32(currentValue) && neighbourValue < 9)
-            return neighbour;
-
-        return null;
-    }
-
+    public bool IsPartOfBassin => Convert.ToInt32(CurrentValue) < 9;
 }
 
 public static class Ext
 {
     public static bool IsSmallerThen(this string current, string? neighbour)
     {
-        bool retval;
-        retval = String.IsNullOrEmpty(neighbour) || (Convert.ToInt32(current) < Convert.ToInt32(neighbour));
-        return retval;
+        return String.IsNullOrEmpty(neighbour) || (Convert.ToInt32(current) < Convert.ToInt32(neighbour));
+    }
+
+    public static bool IsRelevant(this PartPoint currentPoint, List<PartPoint> handledPoints, List<PartPoint> unhandledPoints)
+    {
+        return currentPoint != null
+            && currentPoint.IsPartOfBassin
+            && !currentPoint.IsAlreadyHandled(handledPoints)
+            && !unhandledPoints.Any(x => x.currentRowPosition == currentPoint.currentRowPosition && x.currentPosition == currentPoint.currentPosition);
+    }
+
+    public static bool IsAlreadyHandled(this PartPoint currentPoint, List<PartPoint> handledPoints)
+    {
+        return handledPoints.Any(p => p.currentPosition == currentPoint.currentPosition && p.currentRowPosition == currentPoint.currentRowPosition);
     }
 }
