a5a32454ab521228ac0a9d98188066fa0ad69742
kry.ban <kry.ban@gmail.com>
2021-12-28 12:07:12 +0100

    Day 10, part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -3,8 +3,47 @@ Console.WriteLine("Hello, World!");
 
 string inputFile = @"./ProjectItems/input.txt";
 List<string> inputRaw = File.ReadAllLines(inputFile).ToList();
+List<string> incompleteLines = new List<string>();
+List<string> incompleteLinesCleaned = new List<string>();
 
 Console.WriteLine($"Day 10, Part 1: Corrupt score is (370407) {CalculateCorruptScore(inputRaw)}");
+Console.WriteLine($"Day 10, Part 2: Corrupt score is (3249889609) {CalculateAutoCompleteScore(inputRaw)}");
+
+long CalculateAutoCompleteScore(List<string> inputRaw)
+{
+    incompleteLines = new List<string>();
+    incompleteLinesCleaned = new List<string>();
+    List<long> autoCompleteScores = new List<long>();
+    int retval = 0;
+    CalculateCorruptScore(inputRaw);
+
+    foreach(var line in incompleteLinesCleaned)
+    {
+        var openingChars = line.Replace(".", string.Empty);
+        long subtotal = 0;
+
+        foreach (var currentOpeningChar in openingChars.Reverse())
+        {
+            subtotal = AutoCompleteScore(subtotal, CorrespondingCloseChar(currentOpeningChar));
+        }
+        autoCompleteScores.Add(subtotal);
+    }
+
+    return autoCompleteScores.OrderBy(x => x).ToList()[autoCompleteScores.Count/2];
+}
+
+long AutoCompleteScore(long subtotal, char c)
+{
+    // ): 1 points.
+    // ]: 2 points.
+    // }: 3 points.
+    // >: 4 points.
+    var retval = subtotal * 5;
+
+    return c.Equals(')') ? retval + 1
+    : c.Equals(']') ? retval + 2
+    : c.Equals('}') ? retval + 3 : retval + 4;
+}
 
 int CalculateCorruptScore(List<string> inputRaw)
 {
@@ -20,34 +59,41 @@ int CalculateCorruptScore(List<string> inputRaw)
         {
             if (current.Equals(')'))
             {
-                retval += ValidateClosingChar('(',current,ref tmp, index, ref rowIsCorrupt);
+                ValidateClosingChar('(',current,ref tmp, index, ref rowIsCorrupt);
             }
             if (current.Equals('}'))
             {
-                retval += ValidateClosingChar('{', current, ref tmp, index, ref rowIsCorrupt);
+                ValidateClosingChar('{', current, ref tmp, index, ref rowIsCorrupt);
             }
             if (current.Equals(']'))
             {
-                retval += ValidateClosingChar('[', current, ref tmp, index, ref rowIsCorrupt);
+                ValidateClosingChar('[', current, ref tmp, index, ref rowIsCorrupt);
             }
             if (current.Equals('>'))
             {
-                retval += ValidateClosingChar('<', current, ref tmp, index, ref rowIsCorrupt);
+                ValidateClosingChar('<', current, ref tmp, index, ref rowIsCorrupt);
             }
 
             if (rowIsCorrupt)
             {
+                retval += CharScore(current);
                 break;
             }
 
             index++;
+        }
+
+        if (!rowIsCorrupt)
+        {
+            incompleteLines.Add(line);
+            incompleteLinesCleaned.Add(tmp);
         }
     }
 
     return retval;
 }
 
-int ValidateClosingChar(char expectedOpener, char current, ref string tmp, int index, ref bool rowIsCorrupt)
+void ValidateClosingChar(char expectedOpener, char current, ref string tmp, int index, ref bool rowIsCorrupt)
 {
     tmp = tmp.Remove(index, 1).Insert(index, ".");
     var substringBack = new string(tmp.Substring(0, index).Reverse().ToArray());
@@ -59,8 +105,7 @@ int ValidateClosingChar(char expectedOpener, char current, ref string tmp, int i
         {
             rowIsCorrupt = true;
             Console.WriteLine($"Corrupt at {substringBack[i]}. Expected '{CorrespondingCloseChar(substringBack[i])}', but found {current}.");
-            return CorruptCharScore(current);
-            break;
+            return;
         }
         else if (substringBack[i] != '.' && substringBack[i] == expectedOpener)
         {
@@ -74,11 +119,9 @@ int ValidateClosingChar(char expectedOpener, char current, ref string tmp, int i
 
     var openerIndex = substringBack.Length - 1 - i;
     tmp = tmp.Remove(openerIndex, 1).Insert(openerIndex, ".");
-
-    return 0;
 }
 
-int CorruptCharScore(char c)
+int CharScore(char c)
 {
     // ): 3 points.
     // ]: 57 points.
@@ -89,9 +132,9 @@ int CorruptCharScore(char c)
     : c.Equals('}') ? 1197 : 25137;
 }
 
-char CorrespondingCloseChar(char v)
+char CorrespondingCloseChar(char openingChar)
 {
-    return v.Equals('{') ? '}'
-        : v.Equals('(') ? ')'
-        : v.Equals('[') ? ']' : '>';
+    return openingChar.Equals('{') ? '}'
+        : openingChar.Equals('(') ? ')'
+        : openingChar.Equals('[') ? ']' : '>';
 }
